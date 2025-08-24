996775cb1b8cccb1d9d00f30997b7001afdc690b
A7X <132829627+A7X71@users.noreply.github.com>
2025-08-24 03:01:56 +0100

    Updated: Added Bubble sort for no reason! just for practise maybe?

--- export.py
+++ export.py
@@ -1,6 +1,26 @@
-# Test Code
+# I'll try doing bubble sort or something lol
 
-print("Hello, Github")
+int_array = [7,6,2,4,6,7,1,8,9]
 
-for i in range(5):
-    print("Hello! " + (str(i+1)))
+def BubbleSort(arr):
+    passes = 0
+    swaps = 0
+    for p in range(len(int_array)): # p = pass
+        for s in range(len(int_array)): # Nested for loop n^2 complexity -- Worst time complexity but bear with me.
+            if s != int_array.index(int_array[-1]) and (arr[s] > arr[s+1]):
+                arr[s], arr[s+1] = arr[s+1], arr[s];
+                swaps += 1
+            elif s == int_array.index(int_array[-1]): # Reached the last element
+                # start a new pass
+                break;
+
+        passes += 1
+    
+    print("Sorted in: " + str(passes) + " passes, and " + str(swaps), "swaps.")
+
+    return arr
+
+BubbleSort(int_array)
+
+print(int_array)
+
