40253868163c84911a8a22d2c35c41a49ae2d7b6
A7X <132829627+A7X71@users.noreply.github.com>
2025-08-25 16:36:23 +0100

    OOP update

--- OOP.py
+++ OOP.py
@@ -4,9 +4,9 @@ class Universe:
         self.galaxies = galaxies
         self.stars = galaxies * 100000000 # approx
     
-    def CountUniverseItems(self):
-        print(f"There are {self.galaxies:,} galaxies and {self.stars:,} stars")
+    def __str__(self):
+        return f"There are {self.galaxies:,} galaxies and {self.stars:,} stars"
 
 Uni = Universe(200000000000)
 
-print(Uni.CountUniverseItems())
+print(Uni)
